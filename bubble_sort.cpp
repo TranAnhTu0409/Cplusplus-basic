49032c494d3ca170bab7689bb9b14572a079fe88
TranAnhTu0409 <104290757+TranAnhTu0409@users.noreply.github.com>
2023-05-28 17:12:12 +0700

    Add files via upload

---
+++ bubble_sort.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+
+using namespace std;
+
+void bubbleSort(int arr[], int size) {
+	for(int i = 0; i < size; i++) {
+		for(int j = i + 1; j < size; j++) {
+			if(arr[i] > arr[j]) {
+				swap(arr[i], arr[j]);
+			}
+		}
+	}
+}
+
+void printArray(int arr[], int size) {
+	for(int i = 0; i < size; i++) {
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
+
+main() {
+	int size;
+	cout << "\nEnter the size of the array: ";
+	cin >> size;
+	
+	int arr[size];
+	cout << "\nEnter the elements of the array: \n";
+	for(int i = 0; i < size; i++) {
+		cin >> arr[i];
+	}
+	
+	cout << "Array before sorting: ";
+	printArray(arr, size);
+	
+	bubbleSort(arr, size);
+	
+	cout << "Array after sorting: ";
+	printArray(arr, size);
+	return 0;
+}
