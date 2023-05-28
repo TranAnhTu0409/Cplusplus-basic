f19bb79db6fc2316c8ec328561cd6abebf3166bb
TranAnhTu0409 <104290757+TranAnhTu0409@users.noreply.github.com>
2023-05-28 10:12:16 +0700

    Add files via upload

---
+++ find_primes.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+bool isPrime(int num) {
+	if(num < 2) {
+		return false;
+	}
+	
+	for(int i = 2; i <= sqrt(num); i++) {
+		if(num % i == 0) {
+			return false;
+		}
+	}
+	return true;
+}
+
+void findPrimes(int start, int end) {
+	cout << "\nEnter number from " << start << " to " << end << " is: ";
+	for(int num = start; num <= end; num++) {
+		if(isPrime(num)) {
+			cout << num << " ";
+		}
+	}
+	cout << endl;
+}
+main() {
+	int start, end;
+	cout << "\nEnter start: "; cin >> start;
+	cout << "\nEnter end: "; cin >> end;
+	
+	findPrimes(start, end);
+	return 0;
+}
