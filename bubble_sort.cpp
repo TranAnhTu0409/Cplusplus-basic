31f9f0026e0ed69c3bb50df6809435a64a384040
TranAnhTu0409 <104290757+TranAnhTu0409@users.noreply.github.com>
2023-06-12 18:04:59 +0700

    Update bubble_sort.cpp

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 void bubbleSort(int arr[], int size) {
-	for(int i = 0; i < size; i++) {
-		for(int j = i + 1; j < size; j++) {
+	for(int i = 0; i < size - 1; i++) {
+		for(int j = i + 1; j < size - 1; j++) {
 			if(arr[i] > arr[j]) {
 				swap(arr[i], arr[j]);
 			}
